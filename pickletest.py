1c855944b6323504aa4a4db127ced3186afcb795
Telakos <tel.akos.2@gmail.com>
2016-10-24 15:21:27 +0200

    v0.0003

--- pickletest.py
+++ pickletest.py
@@ -1,20 +1,18 @@
 import pickle
-operation =(str(input("Please specify operation: ")))
-todolist = pickle.load(open("test", "rb"))
-if operation == "a":
-    todolist.append(str(input("input your task: ")))
-    pickle.dump(todolist, open("test", "wb"))
-if operation == "v":
-    todolist = pickle.load(open("test", "rb"))
-    print(todolist)
-if operation == "r":
-    todolist = pickle.load(open("test", "rb"))
-    remid = int(input("which task do you want to remove? "))
-    todolist.remove(todolist[remid])
-    pickle.dump(todolist, open("test", "wb"))
-
-    
-
-
-
-
+while True:
+        operation =(str(input("Please specify operation: ")))
+        todolist = pickle.load(open("test", "rb"))
+        #marklist = pickle.load(open("mark", "rb"))
+        if operation == "a":
+            todolist.append(str(input("input your task: ")))
+            pickle.dump(todolist, open("test", "wb"))
+        elif operation == "v":
+            todolist = pickle.load(open("test", "rb"))
+            print(todolist)
+        elif operation == "r":
+            todolist = pickle.load(open("test", "rb"))
+            remid = int(input("which task do you want to remove? "))
+            todolist.remove(todolist[remid])
+            pickle.dump(todolist, open("test", "wb"))
+        elif operation == "x":
+            break
