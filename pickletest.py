b07b8e5da67feadb9e2be1032176d3a1505ad187
Telakos <tel.akos.2@gmail.com>
2016-10-24 14:15:39 +0200

    added first test and script

---
+++ pickletest.py
@@ -0,0 +1,9 @@
+import pickle
+operation =(str(input("Please specify operation: ")))
+todolist = []
+if operation == "a":
+    todolist.append(str(input("input your task: ")))
+    print(todolist[0])
+
+
+
