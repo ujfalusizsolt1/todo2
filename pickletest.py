770cee333d01afe5a0e96d76660b4596a0144cfe
Telakos <tel.akos.2@gmail.com>
2016-10-24 14:53:46 +0200

    added remove func

--- pickletest.py
+++ pickletest.py
@@ -7,6 +7,12 @@ if operation == "a":
 if operation == "v":
     todolist = pickle.load(open("test", "rb"))
     print(todolist)
+if operation == "r":
+    todolist = pickle.load(open("test", "rb"))
+    remid = int(input("which task do you want to remove? "))
+    todolist.remove(todolist[remid])
+    pickle.dump(todolist, open("test", "wb"))
+
     
 
 
